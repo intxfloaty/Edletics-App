5d89b4dcc7c915c89a18c6e5a770e994a4c5b9d2
Pravesh <praveshjha5@gmail.com>
2023-02-03 18:15:26 +0530

    added image picker for selecting profile images

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Pressable, Image } from 'react-native'
 import React, { useState } from 'react'
 import CustomInput from '../components/CustomInput'
 import CustomButton from '../components/CustomButton'
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const ProfileScreen = () => {
+  const [image, setImage] = useState(null);
   const [fullName, setFullName] = useState("")
   const [date, setDate] = useState(new Date(1598051730000));
   const [emailAddress, setEmailAddress] = useState("")
   const [location, setLocation] = useState("")
+
+
+  const choseFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true
+    }).then(image => {
+      console.log(image);
+      setImage(image.path)
+    });
+  }
 
 
   const showDatepicker = () => {
@@ -30,39 +44,45 @@ const ProfileScreen = () => {
   };
 
   return (
-      <ScrollView contentContainerStyle={styles.parent} showsVerticalScrollIndicator={false}>
-        <Pressable style={styles.profileImageContainer}></Pressable>
-        <View style={styles.profileInfoContainer} >
-          <Text style={styles.inputText}>FULL NAME</Text>
-          <CustomInput
-            value={fullName}
-            setValue={(text) => setFullName(text)}
-          />
-          <Text style={styles.inputText}>DATE OF BIRTH</Text>
-          <CustomInput
-            onPressIn={showDatepicker}
-            showSoftInputOnFocus={false}
-            value={date.toLocaleString().replace(", 4:45:30 AM", " ")}
-          />
-          <Text style={styles.inputText}>EMAIL ADDRESS</Text>
-          <CustomInput
-            keyboardType="email-address"
-            value={emailAddress}
-            inputName="emailAddress"
-            setValue={(text) => setEmailAddress(text)}
-          />
-          <Text style={styles.inputText}>LOCATION</Text>
-          <CustomInput
-            value={location}
-            inputName="location"
-            setValue={(text) => setLocation(text)}
-          />
-          <CustomButton
-            text="SUBMIT"
-          // onPress={onSubmitPressed} 
-          />
-        </View>
-      </ScrollView >
+    <ScrollView contentContainerStyle={styles.parent} showsVerticalScrollIndicator={false}>
+      <Pressable
+        style={styles.profileImageContainer}
+        onPress={choseFromLibrary}>
+        <Image source={{
+          uri: image,
+        }} style={styles.profileImage} />
+      </Pressable>
+      <View style={styles.profileInfoContainer} >
+        <Text style={styles.inputText}>FULL NAME</Text>
+        <CustomInput
+          value={fullName}
+          setValue={(text) => setFullName(text)}
+        />
+        <Text style={styles.inputText}>DATE OF BIRTH</Text>
+        <CustomInput
+          onPressIn={showDatepicker}
+          showSoftInputOnFocus={false}
+          value={date.toLocaleString().replace(", 4:45:30 AM", " ")}
+        />
+        <Text style={styles.inputText}>EMAIL ADDRESS</Text>
+        <CustomInput
+          keyboardType="email-address"
+          value={emailAddress}
+          inputName="emailAddress"
+          setValue={(text) => setEmailAddress(text)}
+        />
+        <Text style={styles.inputText}>LOCATION</Text>
+        <CustomInput
+          value={location}
+          inputName="location"
+          setValue={(text) => setLocation(text)}
+        />
+        <CustomButton
+          text="SUBMIT"
+        // onPress={onSubmitPressed} 
+        />
+      </View>
+    </ScrollView >
   )
 }
 
@@ -80,6 +100,12 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     height: 200,
     width: 200,
+    borderRadius: 100,
+    borderWidth: 1,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
     borderRadius: 100,
     borderWidth: 1,
   },
