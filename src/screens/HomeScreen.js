6e8c6dd1e4cfda196213a6d504f2295678220012
Pravesh <praveshjha5@gmail.com>
2023-02-05 15:37:33 +0530

    added menu options

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CustomButton from '../components/CustomButton'
+import MainMenuOptions from '../components/MainMenuOptions'
 
 const HomeScreen = () => {
   return (
-    <CustomButton text="sign out" onPress={() => {
-      auth()
-        .signOut()
-        .then(() => console.log("user logged out"))
-    }} />
+    <View style={styles.parent}>
+      <MainMenuOptions />
+      {/* <CustomButton text="sign out" onPress={() => {
+        auth()
+          .signOut()
+          .then(() => console.log("user logged out"))
+      }} /> */}
+    </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  parent: {
+    height: "100%",
+    backgroundColor: "#101112",
+  },
+})
