c77a38ef22dc77a1c8270317444ea6e9512a39a9
Pravesh <praveshjha5@gmail.com>
2023-02-27 11:35:19 +0530

    so that material top tabs from react navigation is used to switch bw tabs

--- src/screens/TournamentScreen.js
+++ src/screens/TournamentScreen.js
@@ -1,19 +1,36 @@
 import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, { useState } from 'react'
-import Icon from 'react-native-vector-icons/Ionicons'
-import CustomScrollableTabs from '../components/CustomScrollableTabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Details from './tournament/Details';
+import Matches from './tournament/Matches';
+import Standings from './tournament/Standings';
+import Stats from './tournament/Stats';
+import TopTeams from './tournament/TopTeams';
+
+
+const Tab = createMaterialTopTabNavigator()
 
 const TournamentScreen = () => {
 
   return (
-    <>
-      <CustomScrollableTabs />
-    </>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: { fontSize: 12, color:"white" },
+        tabBarItemStyle: { width: 100 },
+        tabBarStyle: { backgroundColor: "#202224" },
+        tabBarScrollEnabled: true
+      }}>
+        <Tab.Screen name='Details' component={Details} />
+        <Tab.Screen name='Matches' component={Matches} />
+        <Tab.Screen name='Standings' component={Standings} />
+        <Tab.Screen name='Stats' component={Stats} />
+        <Tab.Screen name='TopTeams' component={TopTeams} />
+    </Tab.Navigator>
   )
 }
 
 export default TournamentScreen
 
 const styles = StyleSheet.create({
-  
+
 })
---
+++ src/screens/tournament/Standings.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Standings = () => {
+  return (
+    <View>
+      <Text>Standings</Text>
+    </View>
+  )
+}
+
+export default Standings
+
+const styles = StyleSheet.create({})
